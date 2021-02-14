baa0071788fd6c18de4e405c449f361590042342
Zico Deng <zicodeng@gmail.com>
2021-02-14 15:12:04 -0800

    Capitalize first letter in test name

--- test/index.test.ts
+++ test/index.test.ts
@@ -2,18 +2,18 @@ import { generateSessionToken, validateSessionToken } from '../src';
 
 const SIGNING_KEY = 'My secret';
 
-test('unmodified session token is valid', () => {
+test('Unmodified session token is valid', () => {
   const sessionToken = generateSessionToken(SIGNING_KEY);
   expect(validateSessionToken(sessionToken, SIGNING_KEY)).toBe(true);
 });
 
-test('modified session token is invalid', () => {
+test('Modified session token is invalid', () => {
   const sessionToken = generateSessionToken(SIGNING_KEY);
   const modifiedSessionToken = 'hello' + sessionToken;
   expect(validateSessionToken(modifiedSessionToken, SIGNING_KEY)).toBe(false);
 });
 
-test('unmodified session token with different signing key is invalid', () => {
+test('Unmodified session token with different signing key is invalid', () => {
   const sessionToken = generateSessionToken('Your secret');
   expect(validateSessionToken(sessionToken, SIGNING_KEY)).toBe(false);
 });
